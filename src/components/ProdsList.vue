54d5c3b54aacc39ef7213c5f57a353f145a5d7b4
jeonjaewook <jwodnrj@ilack.co.kr>
2020-05-20 12:28:37 +0900

    axios 설정 및 연결

---
+++ src/components/ProdsList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <div>{{ datas }}</div>
+      <div>{{ datas.fe1 }}</div>
+      <div>{{ datas.fe2 }}</div>
+      <div>aaaaa</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            datas: [],
+        }
+    },
+   created (){
+    //    const url = '';
+   
+    axios.get('https://raw.githubusercontent.com/joshua1988/doit-vuejs/master/data/demo.json')
+            .then(res=> {
+                console.log('succ:'+res);
+                this.datas = res.data;
+            })
+            .catch(err =>{
+                console.log(err);
+            });
+
+   }
+
+}
+</script>
+
+<style>
+
+</style>
